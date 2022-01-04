8db5070db6d5c74a03011e5e4a80948be6ebad0d
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-01-04 17:01:04 +0300

    Auth, middleware, global store

---
+++ atoms/playlist.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "3h8PSvzoY85NhAVULw27PP",
+});
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
